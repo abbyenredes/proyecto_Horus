72f1b1199dd38669444edc27886b580f709be086
abby <126714712+abbyenredes@users.noreply.github.com>
2025-06-05 12:03:28 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -2,7 +2,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 # Configuración de conexión a MongoDB
-uri = "mongodb+srv://admin:Password@cluster0.j9n8ymo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://admin:"
 client = MongoClient(uri)
 # Base de datos y colección
 db = client["madrid42"]  # replace with your DB name
